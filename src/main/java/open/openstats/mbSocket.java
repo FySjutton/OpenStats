0739bc46b3a099de4bc92b7b2dbe1dc8d618e0ac
Fy17 <rubik.sten@gmail.com>
2024-08-17 23:53:34 +0200

    New self playing feature in Musical Blocks

---
+++ src/main/java/open/openstats/mbSocket.java
@@ -0,0 +1,127 @@
+package open.openstats;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.sipgate.mp3wav.Converter;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.AudioStream;
+import net.minecraft.text.Text;
+
+import javax.sound.sampled.*;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.*;
+
+import static open.openstats.openStats.LOGGER;
+
+public class mbSocket {
+    private Socket socket;
+
+    private Clip audioClip;
+    private long resumeTime = 0;
+
+    public void loadAudio(String urlString) {
+        try {
+            LOGGER.info("[OpenStats] Fetching audio track.");
+            // Fetching and converting
+            URL url = new URL(urlString);
+            InputStream audioInput = url.openStream();
+            ByteArrayOutputStream wavOutput = new ByteArrayOutputStream();
+            Converter converter = Converter.convertFrom(audioInput);
+            converter.to(wavOutput);
+            audioInput.close();
+            InputStream audioInputStream = new ByteArrayInputStream(wavOutput.toByteArray());
+
+            // Play
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioInputStream);
+            audioInputStream.close();
+
+            AudioFormat format = audioStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            audioClip = (Clip) AudioSystem.getLine(info);
+
+            // this will only play it once, no loop, but hopefully that's fine :P
+            audioClip.open(audioStream);
+            audioStream.close();
+        } catch (Exception e) {
+            LOGGER.error("Failed to load audio for MB! " + e);
+            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("§cCould not fetch the audio track for this round."));
+        }
+    }
+
+    public void pause() {
+        if (audioClip != null) {
+            LOGGER.info("[OpenStats] MB music paused.");
+            resumeTime = audioClip.getMicrosecondPosition();
+            audioClip.stop();
+        }
+    }
+
+    public void resume() {
+        if (audioClip != null) {
+            LOGGER.info("[OpenStats] MB music resumed.");
+            audioClip.setMicrosecondPosition(resumeTime);
+            audioClip.start();
+        }
+    }
+
+    public void end() {
+        if (audioClip != null) {
+            LOGGER.info("[OpenStats] MB music ended.");
+            audioClip.stop();
+            audioClip.close();
+        }
+        audioClip = null;
+    }
+
+    public void setupSocket(String playerName) {
+        IO.Options options;
+
+        Map<String, String> authMap = new HashMap<>();
+        authMap.put("name", playerName);
+
+        options = IO.Options.builder()
+                .setAuth(authMap)
+                .setSecure(true)
+                .setReconnection(true)
+                .build();
+
+        try {
+            socket = IO.socket("https://mbn.k55.se", options);
+        } catch (URISyntaxException e) {
+            LOGGER.error("Could not create MB socket. Autoplay in Musical Blocks will not work, please restart your game to fix the problem.");
+            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("§cCould not create MB socket. Autoplay in Musical Blocks will not work, please restart your game to fix the problem."));
+            return;
+        }
+
+        socket.on(Socket.EVENT_CONNECT, args -> System.out.println("Connected to SOCKET server"));
+
+        socket.on("song", args -> {
+            JsonElement arg = JsonParser.parseString(Arrays.toString(args));
+            String musicTrack = arg.getAsJsonArray().get(0).getAsJsonObject().get("id").getAsString();
+            resumeTime = 0; // important, wasted an hour on this :(
+            loadAudio("https://antivpn.k55.se/tracks/" + musicTrack);
+        });
+
+        socket.on("play", args -> {
+            resume();
+        });
+
+        socket.on("end", args -> {
+            end();
+        });
+
+        socket.on("pause", args -> {
+            pause();
+        });
+        socket.connect();
+    }
+
+    public void closeSocket() {
+        LOGGER.info("Closing socket...");
+        socket.disconnect();
+    }
+}
--- src/main/java/open/openstats/openStats.java
+++ src/main/java/open/openstats/openStats.java
@@ -7,28 +7,73 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.socket.client.SocketOptionBuilder;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.CommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import open.openstats.informationScreen.infoScreen;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.Collections;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+import java.util.*;
 import java.util.stream.Collectors;
+
 
 public class openStats implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("openstats");
+	private mbSocket musicSocket;
+	private Thread socketThread;
+	private String playerName = "";
+
+	private void createMusicSocket(String playerName) {
+		LOGGER.info("Creating new MB socket.");
+		if (musicSocket != null) {
+			musicSocket.closeSocket();
+		}
+		if (socketThread != null) {
+			socketThread.interrupt();
+		}
+
+		musicSocket = new mbSocket();
+		socketThread = new Thread(() -> {
+			musicSocket.setupSocket(MinecraftClient.getInstance().player.getName().getString());
+		});
+
+		socketThread.start();
+	}
 
 	@Override
 	public void onInitialize() {
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			ClientPlayerEntity player = MinecraftClient.getInstance().player;
+			if (player != null) {
+				if (!playerName.equals(player.getName().getString())) {
+					playerName = player.getName().getString();
+					createMusicSocket(playerName);
+				}
+			}
+		});
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 			LiteralArgumentBuilder<FabricClientCommandSource> lookupCommand = ClientCommandManager.literal("lookup")
 					.executes(createFeedbackExecutor("lookup"))
 					.then(ClientCommandManager.argument("player", StringArgumentType.string())
